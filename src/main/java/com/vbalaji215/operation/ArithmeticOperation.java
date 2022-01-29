6c658116f9928798efd93963087b596f966ca17b
vbalaji215-dev <vbalaji215@outlook.com>
2022-01-29 09:38:50 -0500

    Initial Add

---
+++ src/main/java/com/vbalaji215/operation/ArithmeticOperation.java
@@ -0,0 +1,17 @@
+package com.vbalaji215.operation;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 29, January 2022
+ * Time: 9:37 AM
+ * Project: operation
+ * Package Name: com.vbalaji215.operation
+ * To change this template use File | Settings | File and Code Template
+ */
+public class ArithmeticOperation {
+
+    public int add(int x, int y){
+        return x + y;
+    }
+}
