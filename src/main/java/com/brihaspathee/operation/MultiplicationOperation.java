5903af9e84c8bb088aa81f804212b252765d75e9
vbalaji215-dev <vbalaji215@outlook.com>
2022-02-02 15:22:08 -0500

    Mulitplication Class added

---
+++ src/main/java/com/brihaspathee/operation/MultiplicationOperation.java
@@ -0,0 +1,17 @@
+package com.brihaspathee.operation;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 02, February 2022
+ * Time: 3:21 PM
+ * Project: operation
+ * Package Name: com.brihaspathee.operation
+ * To change this template use File | Settings | File and Code Template
+ */
+public class MultiplicationOperation {
+
+    public int multiply(int x, int y){
+        return x * y;
+    }
+}
